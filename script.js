f90134a558a65816ac7109f698758643d124a9d9
あっつー！ <xx09fort05nitexx@gmail.com>
2026-06-22 22:44:08 +0900

    ステータス管理とステータスによって色分けする機能と削除機能追加

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//HTMLタグをJSの方で保存
 const animeInput = document.getElementById("animeInput");
 const addbtn =document.getElementById("addBtn");
 const animeList = document.getElementById("animeList");
 
+//ボタンの送信を押すと現在書いてるアニメの名前が送られる
 addbtn.addEventListener("click",function() {
   const animeName = animeInput.value;
 
@@ -12,12 +14,42 @@ addbtn.addEventListener("click",function() {
 
   const li =document.createElement("li");
   li.textContent =animeName;
+  //視聴しているとかのステータス付けをしている
+  li.textContent =animeName +" ";
+  const select =document.createElement("select");
+  select.innerHTML=`
+  <option value="見たい">見たい</option>
+   <option value="視聴中">視聴中</option>
+    <option value="視聴済み">視聴済み</option>
+  `;
+  li.appendChild(select);
+select.addEventListener("change", function() {
+ 
+  //ステータスによって色が変化する
+  if (select.value === "見たい") {
+    li.style.backgroundColor = "lightblue";
+ 
+  } else if (select.value === "視聴中") {
+    li.style.backgroundColor = "lightyellow";
+ 
+  } else if (select.value === "視聴済み") {
+    li.style.backgroundColor = "lightgreen";
+  }
+}); 
+
+  //削除ボタン追加  
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent ="削除";
+  deleteBtn.addEventListener("click",function(){
+    animeList.removeChild(li);
+  });
+  li.appendChild(deleteBtn);
   animeList.appendChild(li);
 
   animeInput.value ="";
 
 })
-
+//Enterキーで送信できるようにしている
 animeInput.addEventListener("keydown",function(e){
 if(e.key ==="Enter"){
   addbtn.click();
